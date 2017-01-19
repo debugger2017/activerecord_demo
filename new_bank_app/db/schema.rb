56f5fa10ebd1c9b70bcd190b9e29e9e6c0207b6e
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-19 18:51:51 +0530

    Added foreign key constraints to depositor

--- new_bank_app/db/schema.rb
+++ new_bank_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170119131153) do
+ActiveRecord::Schema.define(version: 20170119131817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,4 +46,6 @@ ActiveRecord::Schema.define(version: 20170119131153) do
   end
 
   add_foreign_key "accounts", "branches"
+  add_foreign_key "depositors", "accounts"
+  add_foreign_key "depositors", "customers"
 end
