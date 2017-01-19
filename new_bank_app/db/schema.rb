e5d2e56835bb29c34f9af6c66d755544c5ac2f01
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-19 18:54:27 +0530

    Created Loan modle

--- new_bank_app/db/schema.rb
+++ new_bank_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170119131817) do
+ActiveRecord::Schema.define(version: 20170119132236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 20170119131817) do
   create_table "depositors", force: :cascade do |t|
     t.integer  "account_id"
     t.integer  "customer_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "loans", force: :cascade do |t|
+    t.string   "branch_name"
+    t.integer  "amount"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
