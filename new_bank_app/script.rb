8a034e00ad671be8751d71605a4b7dc6d31fe802
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-20 12:09:50 +0530

    Script is again changed

--- new_bank_app/script.rb
+++ new_bank_app/script.rb
@@ -2,8 +2,8 @@ require 'csv'
 
 fields = %w{branch_id amount}
 
-CSV.foreach("/home/debugger2017/activerecord_demo/new_bank_app/data/loans.csv",headers: true) do |row|
+CSV.foreach("/home/debugger2017/activerecord_demo/new_bank_app/data/borrower.csv",headers: true) do |row|
 	rows = row.to_hash
-	a = Loan.new(rows)
+	a = Borrower.new(rows)
 	a.save
 end
