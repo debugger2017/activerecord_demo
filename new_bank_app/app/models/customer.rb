8678f3d3e8f68bca56ea2c757c57c80d914da116
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-20 17:56:34 +0530

    Added associations for loan borrower and customer

--- new_bank_app/app/models/customer.rb
+++ new_bank_app/app/models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < ApplicationRecord
 	has_many :depositors
 	has_many :accounts , through: :depositors
+	has_many :borrowers
+	has_many :loans , through: :borrowers
 end
--- new_bank_app/app/models/loan.rb
+++ new_bank_app/app/models/loan.rb
@@ -1,2 +1,4 @@
 class Loan < ApplicationRecord
+	has_one :borrower
+	has_one :customer , through: :borrower
 end
